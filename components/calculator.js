c73b5fc3975ba4971b1124a8d1c7e6bb672b3136
Pau Perez <saulopefa@gmail.com>
2020-12-27 13:28:10 +0100

    Add up and down times apart from total

--- components/calculator.js
+++ components/calculator.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 
 import Input from '../components/input'
-import { decimalToTime, totalTime } from '../lib/time'
+import { decimalToTime, timings } from '../lib/time'
 
 export default function Calculator() {
-  const [total, setTotal] = useState();
-  const [distance, setDistance] = useState();
-  const [slope, setSlope] = useState();
+  const [total, setTotal] = useState(),
+        [distance, setDistance] = useState(),
+        [slope, setSlope] = useState(),
+        [up_time, setUpTime] = useState(),
+        [down_time, setDownTime] = useState();
 
   function handleDistanceChange(e) {
     e.preventDefault()
@@ -14,7 +16,12 @@ export default function Calculator() {
     const distance = e.target.value;
 
     setDistance(distance)
-    setTotal(totalTime(slope, distance))
+
+    const times = timings(slope, distance);
+
+    setUpTime(times.up_time)
+    setDownTime(times.down_time)
+    setTotal(times.total_time)
   }
 
   function handleSlopeChange(e) {
@@ -23,35 +30,54 @@ export default function Calculator() {
     const slope = e.target.value;
 
     setSlope(slope)
-    setTotal(totalTime(slope, distance))
+
+    const times = timings(slope, distance);
+
+    setUpTime(times.up_time)
+    setDownTime(times.down_time)
+    setTotal(times.total_time)
   }
 
   return (
     <div className="mt-14 max-w-xs mx-auto">
-      <div className="grid gap-6">
-        <Input
-          type="number"
-          name="slope"
-          label="Desnivell"
-          placeholder="0"
-          unit="metres"
-          onChange={handleSlopeChange}
-          value={slope}
-        />
-        <Input
-          type="number"
-          name="distance"
-          label="Distància"
-          placeholder="0"
-          unit="kilòmetres"
-          onChange={handleDistanceChange}
-          value={distance}
-        />
+      <div className="grid gap-10">
+        <div className="grid gap-6">
+          <Input
+            type="number"
+            name="slope"
+            label="Desnivell"
+            placeholder="0"
+            unit="metres"
+            onChange={handleSlopeChange}
+            value={slope}
+          />
+          <Input
+            type="number"
+            name="distance"
+            label="Distància"
+            placeholder="0"
+            unit="kilòmetres"
+            onChange={handleDistanceChange}
+            value={distance}
+          />
+        </div>
 
-        <div>
-          <p className="text-gray-400">Temps total</p>
-          <div className="text-4xl font-medium text-gray-700">
-            {total == null ? "..." : decimalToTime(total)}
+        <div className="flex">
+          <div className="mr-6 space-y-1">
+            <div>
+              <span className="font-medium text-lg text-gray-700">{decimalToTime(up_time)}</span>
+              <span className="ml-1 text-gray-400 text-2xl font-mono">&#8599;</span>
+            </div>
+            <div>
+              <span className="font-medium text-lg text-gray-700">{decimalToTime(down_time)}</span>
+              <span className="ml-1 text-gray-400 text-2xl font-mono">&#8601;</span>
+            </div>
+          </div>
+          <div>
+            <p className="text-gray-400 mt-1">Total</p>
+            <div className="text-4xl font-medium text-gray-700">
+              {decimalToTime(total)}
+            </div>
           </div>
         </div>
       </div>
